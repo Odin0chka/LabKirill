bc46c23c6158035f1acfa13815350da0c3a135db
Alexey <alex5osinin@gmail.com>
2018-03-13 03:01:48 +0700

    Допилено 3 задание первой части С++

--- Lab1_Кирилл/C++/Lab1a3/Lab1C++/Coordinate.cpp
+++ Lab1_Кирилл/C++/Lab1a3/Lab1C++/Coordinate.cpp
@@ -4,50 +4,66 @@
 
 Coordinate::Coordinate()
 {
-	degrees = 0;
-	minutes = 0;
-	seconds = 0;
+	x1 = 0;
+	y1 = 0;
+	z1 = 0;
+	x2 = 0;
+	y2 = 0;
+	z2 = 0;
 }
 
 Coordinate Coordinate::add(Coordinate one, Coordinate two)
 {
 	Coordinate res;
-	res.degrees = (one.degrees + two.degrees) / 2;
-	res.minutes = (one.minutes + two.minutes) / 2;
-	res.seconds = (one.seconds + two.seconds) / 2;
+	res.x1 = one.x1;
+	res.y1 = one.y1;
+	res.z1 = one.z1;
+	res.x2 = two.x2;
+	res.y2 = two.y2;
+	res.z2 = two.z2;
 	return res;
 }
 
-int Coordinate::RoundDegrees() {
-	int temp = 0;
-	if (seconds >= 30)
-		temp++;
-	if (minutes + temp >= 30)
-		temp++;
-	if (temp == 2)
-		return degrees + 1;
-	else
-		return degrees;
+float Coordinate::LenghtOt() {
+	float len = sqrt(pow(x1 + x2, 2) + pow(y1 + y2, 2) + pow(z1 + z2, 2));
+	return len;
 }
 
 void Coordinate::Display()
 {
-	printf("\tГрадусы: %d\n", degrees);
-	printf("\tМинуты: %d\n", minutes);
-	printf("\tСекунды: %d\n", seconds);
-	printf("\tОкругление: %d\n", RoundDegrees());
+	printf("\tx1: %.2f\n", x1);
+	printf("\ty1: %.2f\n", y1);
+	printf("\tz1: %.2f\n", z1);
+	printf("\tx2: %.2f\n", x2);
+	printf("\ty2: %.2f\n", y2);
+	printf("\tz2: %.2f\n", z2);
+	printf("\tДлина отрезка: %.2f\n", LenghtOt());
 }
 
-void Coordinate::Init(int degrees, int minutes, int seconds)
+void Coordinate::Init(float cx1, float cy1, float cz1, float cx2, float cy2, float cz2)
 {
-	this->degrees = degrees;
-	this->minutes = minutes;
-	this->seconds = seconds;
+	x1 = cx1;
+	y1 = cy1;
+	z1 = cz1;
+	x2 = cx2;
+	y2 = cy2;
+	z2 = cz2;
 }
 
 void Coordinate::Read()
 {
-	scanf_s("%d %d %d", &degrees, &minutes, &seconds);
+	printf("\tx1: ");
+	scanf_s("%f", &x1);
+	printf("\ty1: ");
+	scanf_s("%f", &y1);
+	printf("\tz1: ");
+	scanf_s("%f", &z1);
+	printf("\tx2: ");
+	scanf_s("%f", &x2);
+	printf("\ty2: ");
+	scanf_s("%f", &y2);
+	printf("\tz2: ");
+	scanf_s("%f", &z2);
 }
 
 Coordinate::~Coordinate()
--- Lab1_Кирилл/C++/Lab1a3/Lab1C++/Coordinate.h
+++ Lab1_Кирилл/C++/Lab1a3/Lab1C++/Coordinate.h
@@ -4,13 +4,16 @@ class Coordinate
 public:
 	Coordinate();
 	~Coordinate();
-	void Init(int degrees, int minutes, int seconds);
+	void Init(float cx1, float cy1, float cz1, float cx2, float cy2, float cz2);
 	void Read();
 	void Display();
-	int RoundDegrees();
+	float LenghtOt();
 	static Coordinate add(Coordinate one, Coordinate two);
 private:
-	int degrees;
-	int minutes;
-	int seconds;
+	float x1;
+	float y1;
+	float z1;
+	float x2;
+	float y2;
+	float z2;
 };
